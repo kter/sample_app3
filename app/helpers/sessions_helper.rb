8f8caf396273a05056415a3cdd92a8c431798de4
kter <me@kter.jp>
2016-10-01 17:45:58 +0900

    ログアウト時記憶トークン等も削除するようにした

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,6 +27,7 @@ module SessionsHelper
 
   # 現在のユーザをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -35,5 +36,12 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
