ca21c22f7c8cdcc8bf6d62d2c1d85791aa81badc
kter <me@kter.jp>
2016-10-01 15:30:34 +0900

    current_userメソッドを実装した

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # 現在ログインしているユーザを返す
+  def current_user
+    # @current_userが存在しない場合のみfind_byが動く
+    # findでは例外が返る時があるのでfind_byを使う
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
