5f3413a13e6874116de8ef420c89417e3119408e
kter <me@kter.jp>
2016-10-01 17:39:18 +0900

    current_userメソッドを記録トークンに対応した

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,15 @@ module SessionsHelper
   def current_user
     # @current_userが存在しない場合のみfind_byが動く
     # findでは例外が返る時があるのでfind_byを使う
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ユーザがログインしていればtrue、その他ならfalseを返す
