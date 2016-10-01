a67359ca48abc78b1e084de0d2f93b1b8aba291c
kter <me@kter.jp>
2016-10-01 17:41:10 +0900

    User.digestでダイジェスト生成の処理が間違えていたのを修正した

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   # 与えられた文字列のハッシュ値を返す
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
