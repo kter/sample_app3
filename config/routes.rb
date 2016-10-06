d35f4349fec97cd69097cea2d0982310f92e9b47
kter <me@kter.jp>
2016-10-07 00:06:37 +0900

    パスワード再発行用のントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
